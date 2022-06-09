7f159112987fbae0727bf7f8009b608a4cd49f7e
afiistiqlal <afiistiqlal@gmail.com>
2022-06-09 23:37:37 +0700

    [Update] Promise Output using filter

--- promise.js
+++ promise.js
@@ -3,10 +3,8 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 const promiseOutput = async (emosi) => {
   try {
-    let jumlahYangEmosi = 0;
     const arrEmosi = (await promiseTheaterIXX()).concat((await promiseTheaterVGC()));
-    arrEmosi.forEach(element => {(element.hasil === emosi) ? jumlahYangEmosi++ : ''});
-    return jumlahYangEmosi;
+    return arrEmosi.filter(element => (element.hasil === emosi)).length;
   } catch (error) {
     console.error(error);
   }
