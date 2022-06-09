888b75d9dad814249040b1be11f20698e103d679
afiistiqlal <afiistiqlal@gmail.com>
2022-06-09 23:06:21 +0700

    [New] Promise Output

--- promise.js
+++ promise.js
@@ -1,7 +1,12 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let jumlahYangEmosi = 0;
+  const arrEmosi = (await promiseTheaterIXX()).concat((await promiseTheaterVGC()));
+  arrEmosi.forEach(element => {(element.hasil === emosi) ? jumlahYangEmosi++ : ''});
+  return jumlahYangEmosi++;
+};
 
 module.exports = {
   promiseOutput,
