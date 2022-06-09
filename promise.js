579fc5dd51cda2c24ded3f3395e5a884353abab2
afiistiqlal <afiistiqlal@gmail.com>
2022-06-09 23:16:54 +0700

    [Update] Promise Output try catch

--- promise.js
+++ promise.js
@@ -2,10 +2,14 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 const promiseOutput = async (emosi) => {
-  let jumlahYangEmosi = 0;
-  const arrEmosi = (await promiseTheaterIXX()).concat((await promiseTheaterVGC()));
-  arrEmosi.forEach(element => {(element.hasil === emosi) ? jumlahYangEmosi++ : ''});
-  return jumlahYangEmosi;
+  try {
+    let jumlahYangEmosi = 0;
+    const arrEmosi = (await promiseTheaterIXX()).concat((await promiseTheaterVGC()));
+    arrEmosi.forEach(element => {(element.hasil === emosi) ? jumlahYangEmosi++ : ''});
+    return jumlahYangEmosi;
+  } catch (error) {
+    return error;
+  }
 };
 
 module.exports = {
